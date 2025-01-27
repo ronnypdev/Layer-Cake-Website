58b367affd9e2ba8c77ea61808db78fc2350cceb
Ronny Perez <ronnypdev@gmail.com>
2025-01-26 19:22:37 -0500

    Implemented starter files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sanity/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
-} satisfies Config;
+};
+export default config;
