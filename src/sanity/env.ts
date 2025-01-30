54bf8d973cff2c405fbb871857cf8051787e1594
Ronny Perez <ronnypdev@gmail.com>
2025-01-30 09:35:07 -0500

    Fixed api variables

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.SANITY_STUDIO_API_VERSION || '2025-01-27'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-27'
 
 export const dataset = assertValue(
   process.env.SANITY_STUDIO_DATASET,
